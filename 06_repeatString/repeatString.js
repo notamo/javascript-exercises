2d3b9786632153684c20a8ddb6e8b3b67ef1e112
notamo <203415281+notamo@users.noreply.github.com>
2025-10-27 19:05:37 +0100

    Complete 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let resString = "";
+    while (num > 0) {
+        resString += str;
+        num--; 
+    }
+    return resString;
 };
 
 // Do not edit below this line
