346b8d7052d6ab93efe951dfd8917eaf15499c27
notamo <203415281+notamo@users.noreply.github.com>
2025-10-27 19:42:07 +0100

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let prunedArr = arr.slice();
+
+    for (elementToRemove of args) {
+        prunedArr = prunedArr.filter(element => element !== elementToRemove);
+    }
+
+    return prunedArr;
 };
 
 // Do not edit below this line
