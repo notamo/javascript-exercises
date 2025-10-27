ed1de4f2b0d4a61c2eec8c7f7855c20e9996cf89
notamo <203415281+notamo@users.noreply.github.com>
2025-10-27 19:51:32 +0100

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    if (a !== Math.floor(a) || b !== Math.floor(b)) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
