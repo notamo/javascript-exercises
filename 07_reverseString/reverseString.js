a856b6696b901b924cad05023822c453210784d7
notamo <203415281+notamo@users.noreply.github.com>
2025-10-27 19:25:23 +0100

    Complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(toReverse) {
+    let res = "";
 
+    for (let i = toReverse.length-1; i >= 0; i--) {
+        res += toReverse[i];
+    }
+
+    return res;
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
