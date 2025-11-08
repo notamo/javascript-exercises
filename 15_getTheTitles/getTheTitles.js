c0072a4688f6b740d90b0e0c1b428d9c68d5c060
notamo <203415281+notamo@users.noreply.github.com>
2025-11-08 19:54:08 +0100

    Complete 15_getTheTitles exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,20 @@
-const getTheTitles = function() {
+/*
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
 
+]
+getTheTitles(books) // ['Book','Book2']
+*/
+
+const getTheTitles = function(arr) {
+    return arr.map((book) => book.title);
 };
 
 // Do not edit below this line
