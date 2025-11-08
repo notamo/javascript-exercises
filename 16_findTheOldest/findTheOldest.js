a161ddf4af9b2c2ba5318f9961118b59cb38181a
notamo <203415281+notamo@users.noreply.github.com>
2025-11-08 20:23:29 +0100

    Complete 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let currOldest = {obj: {}, age: 0};
 
+    for (person of arr) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        
+        const age = person.yearOfDeath - person.yearOfBirth;
+        if (age > currOldest.age) {
+            currOldest = {
+                obj: person,
+                age: age,
+            }
+        }
+    
+    }
+
+    return currOldest.obj;
 };
 
 // Do not edit below this line
