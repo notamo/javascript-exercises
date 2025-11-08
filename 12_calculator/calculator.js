354d2f230693b430ebaa5f85be490f1435ff8cb2
notamo <203415281+notamo@users.noreply.github.com>
2025-11-08 19:12:37 +0100

    Complete 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, element) => acc + element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, element) => acc * element);
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	return b ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 ) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
