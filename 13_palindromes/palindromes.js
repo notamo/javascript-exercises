87f438f89eb6eabefd0db2a63166ecf02e18cd8d
notamo <203415281+notamo@users.noreply.github.com>
2025-11-08 19:44:31 +0100

    Complete 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const sClean = s
+        .split("")
+        .filter((c) => 
+                ((c.charCodeAt(0) >= 48) && (c.charCodeAt(0) <= 57 ))  ||
+                ((c.charCodeAt(0) >= 65) && (c.charCodeAt(0) <= 90 ))  ||
+                ((c.charCodeAt(0) >= 97) && (c.charCodeAt(0) <= 122))   
+        )
+        .join("")
+        .toLowerCase();
 
+    const sCleanRev = sClean.split("").reverse().join(""); 
+    return sClean === sCleanRev;
 };
 
 // Do not edit below this line
